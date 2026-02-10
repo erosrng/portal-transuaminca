d4cfaf8abb6e47ab82cdf62261c0652ff04e5301
root <root@localhost.localdomain>
2026-02-10 10:56:03 -0400

    Urls bien

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,57 +12,24 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 // ***********************************************
 
-//URL PRACTICA INTERNA 
-/* export const API_URL = 'http://10.0.100.2/practica/api/';
-export const URLSOLA = 'http://10.0.100.2/';
-export const PROTEO_URL_ALONE = 'http://10.0.100.2/practica/';
+//URL PARA TRABAJAR
+export const API_URL = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transuaminca/api/';
+export const URLSOLA = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/';
+export const PROTEO_URL_ALONE = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transuaminca/';
 
-//PRACTICA SOLO PRUEBAS LOCALES
-export const API_URLINTER = 'http://10.0.100.2/practica/api/';
-export const URLSOLAINTER = 'http://10.0.100.2/';
-export const PROTEO_URL_ALONEINTER = 'http://10.0.100.2/practica/';  */
-
-//URL PRACTICA EXTERNA
-
-/* export const API_URL = 'https://d2wnvkodoh477y.cloudfront.net/practica/api/';
-export const URLSOLA = 'https://d2wnvkodoh477y.cloudfront.net/';
-export const PROTEO_URL_ALONE = 'https://d2wnvkodoh477y.cloudfront.net/practica/';
-
- export const API_URLINTER = 'https://d2wnvkodoh4
- // 
- // 77y.cloudfront.net/practica/api/';
-export const URLSOLAINTER = 'https://d2wnvkodoh477y.cloudfront.net/';
-export const PROTEO_URL_ALONEINTER = 'https://d2wnvkodoh477y.cloudfront.net/practica/';  */
-
-//Transuaminca
-/* export const API_URL = 'http://transuaminca.proteoerp.org/transuaminca/api/';
-export const URLSOLA = 'http://transuaminca.proteoerp.org/';
-export const PROTEO_URL_ALONE = 'http://transuaminca.proteoerp.org/transuaminca/';
-
-export const API_URLINTER = 'http://transuaminca.proteoerp.org/transuaminca/api/';
-export const URLSOLAINTER = 'http://transuaminca.proteoerp.org/';
-export const PROTEO_URL_ALONEINTER = 'http://transuaminca.proteoerp.org/transuaminca/'; */
-
-//PRUEBA TRANSUAMINCA
-//Transuaminca
-/* export const API_URL = 'http://transuaminca.proteoerp.org/transu/api/';
-export const URLSOLA = 'http://transuaminca.proteoerp.org/';
-export const PROTEO_URL_ALONE = 'http://transuaminca.proteoerp.org/transu/';
-
-export const API_URLINTER = 'http://transuaminca.proteoerp.org/transu/api/';
-export const URLSOLAINTER = 'http://transuaminca.proteoerp.org/';
-export const PROTEO_URL_ALONEINTER = 'http://transuaminca.proteoerp.org/transu/'; */
+export const API_URLINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transuaminca/api/';
+export const URLSOLAINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/';
+export const PROTEO_URL_ALONEINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transuaminca/'; 
 
 
-
-//PROXY
-export const API_URL = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/api/';
+//PROXY PRACTICA
+/* export const API_URL = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/api/';
 export const URLSOLA = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/';
 export const PROTEO_URL_ALONE = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/';
 
 export const API_URLINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/api/';
 export const URLSOLAINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/';
-export const PROTEO_URL_ALONEINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/';
+export const PROTEO_URL_ALONEINTER = 'https://ancient-meadow-8bfe.proteo-api.workers.dev/transu/'; */
 
 
 //export const API_URL = 'https://d2wnvkodoh477y.cloudfront.net/practica/api/'; 
